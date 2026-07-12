eef2c92ea517af7ac19fab21d57860eca3a7bfac
noxqzq <zhakenj2007@gmail.com>
2026-07-12 16:12:40 +0500

    added simple echo server

---
+++ echo.cpp
@@ -0,0 +1,30 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+
+int main() {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // let us reuse the port immediately after the program exits
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;   // listen on all interfaces
+    addr.sin_port = htons(8080);         // htons = host-to-network byte order
+
+    bind(server_fd, (sockaddr*)&addr, sizeof(addr));
+    listen(server_fd, 16);               // 16 = backlog of pending connections
+    std::cout << "listening on :8080\n";
+
+    while (true) {
+        int conn = accept(server_fd, nullptr, nullptr);  // blocks here
+        char buf[1024];
+        ssize_t n = read(conn, buf, sizeof(buf));
+        write(conn, buf, n);             // echo it straight back
+        close(conn);
+    }
+}
